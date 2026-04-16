0eb9d4f962b8894803296dfa5a87c9749864447d
YashMishra0101 <yashrkm0011@gmail.com>
2026-04-17 01:14:47 +0530

    fix(portfolio): restore favicon and remove leftover pwa assets

--- apps/dashboard/dev-dist/sw.js
+++ apps/dashboard/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-e2640689'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1l1dunsicss"
+    "revision": "0.4kah994647k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
