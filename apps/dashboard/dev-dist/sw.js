510447b53f75551dc7798f552c11f2d7de93329c
YashMishra0101 <yashrkm0011@gmail.com>
2026-08-13 23:38:59 +0530

    style: add Docker icon to tech stack section

--- apps/dashboard/dev-dist/sw.js
+++ apps/dashboard/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.u3mag7ik35c"
+    "revision": "0.f93jacsih0k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
